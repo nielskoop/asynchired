7a2848c679ded81f86ee8848279facb05e41b2a4
Niels Koop <business.klnkoop@gmail.com>
2023-11-28 12:53:10 +0100

    feat: job-list mobile design

--- src/components/JobList.tsx
+++ src/components/JobList.tsx
@@ -1,13 +1,35 @@
 import type { Post } from "@prisma/client";
+import Image from "next/image";
 import React from "react";
 import { api } from "~/utils/api";
 
+export function JobTag(props: { tag: string; type: string }) {
+  return (
+    <div className="mr-2 flex items-center">
+      <Image
+        src={`/JobTags/${props.type}.svg`}
+        height={20}
+        width={20}
+        alt={`${props.tag} ${props.type} tag`}
+      />
+      <span className="text-sm">{props.tag}</span>
+    </div>
+  );
+}
+
 export function JobListing(post: Post) {
   return (
-    <div className="border-b border-slate-300 p-2">
-      <div>{post.title} - 6 days ago</div>
-      <div>
-        {post.company} - {post.location}
+    <div className="border-b border-slate-300 p-3">
+      <div className="mb-2">
+        <span className="font-bold">{post.title}</span>
+        <span className="ml-2 text-xs font-light text-slate-500">
+          6 days ago
+        </span>
+      </div>
+      <div className="flex flex-wrap">
+        {post.company && <JobTag tag={post.company} type={"company"} />}
+        {post.location && <JobTag tag={post.location} type={"location"} />}
+        {post.salary && <JobTag tag={post.salary} type={"salary"} />}
       </div>
     </div>
   );
