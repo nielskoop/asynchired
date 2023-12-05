00021e3cad5ac0dba18301b3175e8f0d3bad7f4f
Libertex13 <nachoplanas.np@gmail.com>
2023-12-05 11:41:42 +0100

    fix:navbar and hamburger buttons interaction with jobLists via context /buttonContext)

--- src/components/userJobsTracker.tsx
+++ src/components/userJobsTracker.tsx
@@ -1,11 +1,15 @@
-import React, { useState } from "react";
+import React from "react";
 import LikedJobsList from "./jobLists/LikedJobsList";
 import DislikedJobsList from "./jobLists/DislikedJobsList";
 import AppliedJobsList from "./jobLists/AppliedJobsList";
+import { useButton } from "~/context/buttonContext";
 
 export const UserJobsTracker: React.FC = () => {
-  // State to keep track of the currently selected list
-  const [selectedList, setSelectedList] = useState("applied");
+  const { selectedList, setSelectedList } = useButton();
+
+  const buttonClass =
+    "flex transform items-center rounded-md border border-blue-700 bg-blue-500 px-4 py-1 font-semibold text-white transition duration-300 ease-in-out";
+  const activeButtonClass = "bg-blue-700 scale-105"; // Additional styles for the active button
 
   return (
     <>
@@ -13,7 +17,9 @@ export const UserJobsTracker: React.FC = () => {
         <ul className="border-b-solid flex flex-row justify-evenly border-b-2 border-[#1A78E6] py-2 pt-2 text-lg">
           <li>
             <button
-              className="flex transform items-center rounded-md border border-blue-700 bg-blue-500 px-4 py-1 font-semibold text-white transition duration-300 ease-in-out hover:scale-105 hover:bg-blue-700"
+              className={`${buttonClass} ${
+                selectedList === "applied" && activeButtonClass
+              }`}
               onClick={() => setSelectedList("applied")}
             >
               <span className="mr-2">📄</span> Applied
@@ -21,7 +27,9 @@ export const UserJobsTracker: React.FC = () => {
           </li>
           <li>
             <button
-              className="flex transform items-center rounded-md border border-blue-700 bg-blue-500 px-4 py-1 font-semibold text-white transition duration-300 ease-in-out hover:scale-105 hover:bg-blue-700"
+              className={`${buttonClass} ${
+                selectedList === "liked" && activeButtonClass
+              }`}
               onClick={() => setSelectedList("liked")}
             >
               <span className="mr-2">❤️</span> Liked
@@ -29,7 +37,9 @@ export const UserJobsTracker: React.FC = () => {
           </li>
           <li>
             <button
-              className="flex transform items-center rounded-md border border-blue-700 bg-blue-500 px-4 py-1 font-semibold text-white transition duration-300 ease-in-out hover:scale-105 hover:bg-blue-700"
+              className={`${buttonClass} ${
+                selectedList === "disliked" && activeButtonClass
+              }`}
               onClick={() => setSelectedList("disliked")}
             >
               <span className="mr-2">👎</span> Disliked
