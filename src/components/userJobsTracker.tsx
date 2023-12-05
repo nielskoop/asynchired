9f71d186d2fbdd6d80980bd90a4873f6b548bf75
Patrik Róbert Horváth <h.patrikrobert@gmail.com>
2023-12-05 11:38:07 +0100

    Update userJobsTracker.tsx

--- src/components/userJobsTracker.tsx
+++ src/components/userJobsTracker.tsx
@@ -2,10 +2,11 @@ import React, { useState } from "react";
 import LikedJobsList from "./jobLists/LikedJobsList";
 import DislikedJobsList from "./jobLists/DislikedJobsList";
 import AppliedJobsList from "./jobLists/AppliedJobsList";
+import { useButton } from "~/context/buttonContext";
 
 export const UserJobsTracker: React.FC = () => {
   // State to keep track of the currently selected list
-  const [selectedList, setSelectedList] = useState("applied");
+ const { stateButton, setStateButton } = useButton();
 
   return (
     <>
@@ -13,24 +14,30 @@ export const UserJobsTracker: React.FC = () => {
         <ul className="border-b-solid flex flex-row justify-evenly border-b-2 border-[#1A78E6] py-2 pt-2 text-lg">
           <li>
             <button
-              className="flex transform items-center rounded-md border border-blue-700 bg-blue-500 px-4 py-1 font-semibold text-white transition duration-300 ease-in-out hover:scale-105 hover:bg-blue-700"
-              onClick={() => setSelectedList("applied")}
+              className={`flex transform items-center rounded-md border border-blue-700 px-4 py-1 font-semibold text-white transition duration-300 ease-in-out hover:scale-105 ${
+                stateButton === "applied" ? "bg-blue-800" : "bg-blue-500 hover:bg-blue-700"
+              }`}
+              onClick={() => setStateButton("applied")}
             >
               <span className="mr-2">📄</span> Applied
             </button>
           </li>
           <li>
             <button
-              className="flex transform items-center rounded-md border border-blue-700 bg-blue-500 px-4 py-1 font-semibold text-white transition duration-300 ease-in-out hover:scale-105 hover:bg-blue-700"
-              onClick={() => setSelectedList("liked")}
+              className={`flex transform items-center rounded-md border border-blue-700 px-4 py-1 font-semibold text-white transition duration-300 ease-in-out hover:scale-105 ${
+                stateButton === "liked" ? "bg-blue-800" : "bg-blue-500 hover:bg-blue-700"
+              }`}
+              onClick={() => setStateButton("liked")}
             >
               <span className="mr-2">❤️</span> Liked
             </button>
           </li>
           <li>
             <button
-              className="flex transform items-center rounded-md border border-blue-700 bg-blue-500 px-4 py-1 font-semibold text-white transition duration-300 ease-in-out hover:scale-105 hover:bg-blue-700"
-              onClick={() => setSelectedList("disliked")}
+              className={`flex transform items-center rounded-md border border-blue-700 px-4 py-1 font-semibold text-white transition duration-300 ease-in-out hover:scale-105 ${
+                stateButton === "disliked" ? "bg-blue-800" : "bg-blue-500 hover:bg-blue-700"
+              }`}
+              onClick={() => setStateButton("disliked")}
             >
               <span className="mr-2">👎</span> Disliked
             </button>
@@ -38,9 +45,9 @@ export const UserJobsTracker: React.FC = () => {
         </ul>
         <div className="overflow-y-auto p-4">
           <div className="mb-4 sm:mx-auto sm:w-4/5">
-            {selectedList === "applied" && <AppliedJobsList />}
-            {selectedList === "liked" && <LikedJobsList />}
-            {selectedList === "disliked" && <DislikedJobsList />}
+            {stateButton === "applied" && <AppliedJobsList />}
+            {stateButton === "liked" && <LikedJobsList />}
+            {stateButton === "disliked" && <DislikedJobsList />}
           </div>
         </div>
       </div>
