03d768f836ed2c021e82ba808e27ad2422c23bdd
Patrik Róbert Horváth <h.patrikrobert@gmail.com>
2023-12-05 09:37:08 +0100

    Add files via upload

---
+++ src/context/profileContext.tsx
@@ -0,0 +1,51 @@
+import React, { createContext, useContext, useState } from "react";
+import type { Dispatch, ReactNode, SetStateAction } from "react";
+// Define the shape of your context data
+interface ProfileDetails {
+    job: string;
+    education: string;
+    techStack: string;
+    location: string;
+    userId: string;
+  }
+  
+  interface ProfileContextType {
+    profileDetails: ProfileDetails;
+    setProfileDetails: Dispatch<SetStateAction<ProfileDetails>>;
+  }
+  
+  // Create the context with an initial dummy value
+  const ProfileContext = createContext<ProfileContextType>({
+    profileDetails: { job: '', education: '', techStack: '', location: '', userId: '' },
+    setProfileDetails: (value: SetStateAction<ProfileDetails>) => {
+      console.log(value);
+    },
+  });
+
+  export const useProfile = () => {
+    const context = useContext(ProfileContext);
+  
+    if (!context) {
+      throw new Error('useProfile must be used within a ProfileProvider');
+    }
+  
+    return context;
+  };
+
+// Type the props for FilterProvider
+interface ProfileProviderProps {
+  children: ReactNode;
+}
+
+export const ProfileProvider: React.FC<ProfileProviderProps> = ({ children }) => {
+  const [profileDetails, setProfileDetails] = useState({job: '', education: '', techStack: '', location: '', userId: ''});
+
+  const value = {
+    profileDetails,
+    setProfileDetails,
+  };
+
+  return (
+    <ProfileContext.Provider value={value}>{children}</ProfileContext.Provider>
+  );
+};
