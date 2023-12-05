fdd155154f9a7b6c6fbef92f0e8950eaf23821b7
raijin <christopherheinzmann@outlook.com>
2023-12-05 17:25:41 +0100

    pre pull commit

--- src/components/savedSearches.tsx
+++ src/components/savedSearches.tsx
@@ -3,13 +3,15 @@ import { JobListSkeleton } from "./jobListSkeleton";
 import { api } from "~/utils/api";
 import { CompanyInputBox } from "./Inputs/CompanyInputBox";
 import { TagWidget } from "~/pages";
+import { useState } from "react";
 
 const SavedSearches: React.FC = () => {
+  const [searchChoice, setSearchChoice] = useState()
   const { userId } = useAuth();
 
   // Conditionally fetching data only if userId is available
-  const { data, isLoading } = userId
-    ? api.user.getApplied.useQuery(userId)
+  const { data: searches, isLoading } = userId
+    ? api.search.getSearches.useQuery()
     : { data: null, isLoading: false };
 
   // Handle loading state
@@ -19,7 +21,11 @@ const SavedSearches: React.FC = () => {
   if (!userId) return <div>Please log in to view liked jobs.</div>;
 
   // Handling case where data fetching failed
-  if (!data) return <div>Something went wrong!</div>;
+  if (!searches) return <div>Something went wrong!</div>;
+
+  function handleSelectSearch() {
+    setSearchChoice
+  }
 
   return (
     <>
@@ -27,19 +33,24 @@ const SavedSearches: React.FC = () => {
         <h2>Your Saved Searches</h2>
       </div>
 
-      <div className="relative mx-8 mb-8 flex max-h-[400px] min-h-[400px] flex-col rounded-lg border-2 border-solid border-[#1A78E6] shadow-lg">
-        <div id="container" className="flex max-h-full flex-row">
-          <div className="overflow-y-auto min-w-fit p-4">
-            <ul className="mb-4 flex flex-col items-start justify-start sm:mx-auto">
-              <li className="t flex w-full flex-row items-center justify-between border-b border-slate-300 shadow-sm">
-                <button className="mr-4 cursor-pointer text-start">
-                  testttttttttttttttttttttttttttttttttt
-                </button>
-                <div className="mr-4 flex flex-col">
+<div className="relative mx-8 mb-8 flex max-h-[400px] min-h-[400px] flex-col rounded-lg border-2 border-solid border-[#1A78E6] shadow-lg">
+  <div id="container" className="flex max-h-full flex-row">
+    <div className="overflow-y-auto max-h-[390px] min-w-fit p-4">
+      <ul className="mb-4 flex flex-col items-start justify-start sm:mx-auto">
+        {searches.map((search) => (
+          <li className="flex w-full flex-row items-center justify-between border-b border-slate-300 shadow-sm">
+            <button className="mr-4 cursor-pointer text-start"
+            onClick={(search) => {handleSelectSearch}}
+            >
+              {search.name}
+            </button>
+            <div className="mr-4 flex flex-col">
                   <button>E</button>
                   <button>D</button>
                 </div>
-              </li>
+              </li>)
+
+              )}
             </ul>
           </div>
           <div id="filtersAndTags" className="w-full">
