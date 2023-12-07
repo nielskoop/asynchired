ad1d4f1b447f6a425dd20a39016e6a1460489fff
Patrik Róbert Horváth <h.patrikrobert@gmail.com>
2023-12-07 10:56:08 +0100

    Update buttonContext.tsx

--- src/context/buttonContext.tsx
+++ src/context/buttonContext.tsx
@@ -1,16 +1,19 @@
-/* eslint-disable @typescript-eslint/no-empty-function */
 import React, { createContext, useContext, useState } from "react";
 import type { Dispatch, ReactNode, SetStateAction } from "react";
 // Define the shape of your context data
 interface ButtonContextType {
   selectedList: string;
   setSelectedList: Dispatch<SetStateAction<string>>;
+  scroll: boolean;
+  setScroll: Dispatch<SetStateAction<boolean>>;
 }
 
 // Create the context with an initial dummy value
 const ButtonContext = createContext<ButtonContextType>({
-  selectedList: "",
+  selectedList: "liked",
   setSelectedList: () => {},
+  scroll: false,
+  setScroll: () => {},
 });
 
 export const useButton = () => useContext(ButtonContext);
@@ -21,11 +24,13 @@ interface ButtonProviderProps {
 }
 
 export const ButtonProvider: React.FC<ButtonProviderProps> = ({ children }) => {
-  const [selectedList, setSelectedList] = useState("");
-
+  const [selectedList, setSelectedList] = useState("liked");
+  const [scroll, setScroll] = useState(false);
   const value = {
     selectedList,
     setSelectedList,
+    scroll,
+    setScroll
   };
 
   return (
